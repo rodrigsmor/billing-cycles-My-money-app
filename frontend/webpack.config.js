36860ead2b8f2ab82858fbae210f7ac326b50444
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-15 17:28:32 -0300

    10 | Import AdminLTE template Dependencies

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,12 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + '/pubic',
+        path: __dirname + '/public',
         filename: './app.js'
-    }, 
+    },
     devServer: {
         port: 8080,
         contentBase: './public',
@@ -15,10 +15,11 @@ module.exports = {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
-            jquery: 'modules/admin-lte/bootstrap/js/bootstrap.js'
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
