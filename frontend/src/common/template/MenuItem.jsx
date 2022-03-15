208f0bcd9cd810161288cd86646b626f61b5c4c5
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-15 17:50:11 -0300

    11 | Create the Menu Sidebar

---
+++ frontend/src/common/template/MenuItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MenuItem = (props) => {
+    return (
+        <li>
+            <a href={props.path}>
+                <i className={`fa fa-${props.icon}`}></i>
+                { props.label }
+            </a>
+        </li>
+    );
+}
+
+export default MenuItem;
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../common/template/dependencies';
 
+import Header from '../common/template/Header';
+import Sidebar from '../common/template/Sidebar';
+
 const App = () => {
     return (
         <div className="wrapper">
-            <h1>App</h1>
+            <Header />
+            <Sidebar />
         </div>
     );
 }
