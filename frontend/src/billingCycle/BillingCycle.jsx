96c66d2105b1d3cb7c98c3d28833c059b2193658
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-16 16:04:14 -0300

    17 | Change tab visibility using Redux

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import Content from "../common/template/Content";
 import ContentHeader from '../common/template/ContentHeader';
-
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import Tabs from "../common/Tab/Tabs";
 import TabsHeader from "../common/Tab/TabsHeader";
 import TabsContent from "../common/Tab/TabsContent";
 import TabHeader from "../common/Tab/TabHeader";
+import TabContent from "../common/Tab/TabContent";
+import { selectTab, showTabs } from '../common/Tab/tabs.actions';
 
 class BillingCycle extends Component {
+    componentWillMount() {
+        this.props.selectTab('tabList');
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+
     render() {
         return (
             <div>
@@ -21,7 +29,10 @@ class BillingCycle extends Component {
                             <TabHeader label={'Excluir'} icon={'trash-o'} target={'tabDelete'}/>
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id={'tabList'}><h1>Listar</h1></TabContent>
+                            <TabContent id={'tabCreate'}><h1>Incluir</h1></TabContent>
+                            <TabContent id={'tabUpdate'}><h1>Alterar</h1></TabContent>
+                            <TabContent id={'tabDelete'}><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>/
@@ -30,4 +41,6 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle;
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch);
+
+export default connect(null, mapDispatchToProps)(BillingCycle);
---
+++ frontend/src/common/Tab/TabContent.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import If from '../Operators/If';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class TabContent extends React.Component {
+    render() {
+        const selected = this.props.tab.selected === this.props.id;
+        const visible = this.props.tab.visible[this.props.id]
+
+        return (
+            <If test={visible}>
+                <div 
+                    id={this.props.id}
+                    className={`tab-pane ${selected ? 'active' : ''}`}
+                >
+                    { this.props.children }
+                </div>
+            </If>
+        );
+    }
+}
+
+const mapStateToProps = state => ({ tab: state.tab })
+const mapDispatchToProps = state => bindActionCreators();
+
+export default connect(mapStateToProps)(TabContent);
