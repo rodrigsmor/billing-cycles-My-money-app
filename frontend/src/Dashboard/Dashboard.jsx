be14294066b805224d6b7eb7ec69312a70deaea3
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-16 13:58:09 -0300

    13 | Develop the Dashboard layout

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import Content from "../common/template/Content";
+import ValueBox from '../common/widget/ValueBox';
+import ContentHeader from "../common/template/ContentHeader";
+import Row from "../common/layout/Row";
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader 
+                    title={'Dashboard'} 
+                    small={'Versão 1.0'} 
+                />
+                <Content>
+                    <Row>
+                        <ValueBox 
+                            cols={'12 4'}
+                            color={'green'}
+                            icon={'bank'}
+                            value={'R$ 10'}
+                            text={'Total de Créditos'}
+                        />
+
+                        <ValueBox 
+                            cols={'12 4'}
+                            color={'red'}
+                            icon={'credit-card'}
+                            value={'R$ 10'}
+                            text={'Total de Débitos'}
+                        />
+
+                        <ValueBox 
+                            cols={'12 4'}
+                            color={'blue'}
+                            icon={'money'}
+                            value={'R$ 10'}
+                            text={'Valor Consolidado'}
+                        />
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
