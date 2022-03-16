3740e6766b7c9e949a033aa767d2bb6a9a647fdd
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-16 13:15:22 -0300

    12 | Implement Page Navigation using react-router

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../common/template/dependencies';
+import Footer from '../common/template/Footer';
 
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
+import Routes from './routes';
 
 const App = () => {
     return (
         <div className="wrapper">
             <Header />
             <Sidebar />
+            <div className="content-wrapper">
+                <Routes />
+            </div>
+            <Footer />
         </div>
     );
 }
