342cd07bfa859b7db5707ad1bb8cba001288c54c
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-16 15:11:14 -0300

    15 | Structure BillingCycle page tabs

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import Content from "../common/template/Content";
+import ContentHeader from '../common/template/ContentHeader';
 
-export default props => {
-    return (
-        <div>
-            <h1>Ciclo de Pagamento</h1>
-        </div>
-    );
+import Tabs from "../common/Tab/Tabs";
+import TabsHeader from "../common/Tab/TabsHeader";
+import TabsContent from "../common/Tab/TabsContent";
+import TabHeader from "../common/Tab/TabHeader";
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title={'Ciclos de Pagamentos'} small={'Cadastro'} />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label={'Listar'} icon={'bars'} target={'tabList'}/>
+                            <TabHeader label={'Incluir'} icon={'plus'} target={'tabCreate'}/>
+                            <TabHeader label={'Alterar'} icon={'pencil'} target={'tabUpdate'}/>
+                            <TabHeader label={'Excluir'} icon={'trash-o'} target={'tabDelete'}/>
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                </Content>/
+            </div>
+        )
+    }
 }
+
+export default BillingCycle;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,7 +6,9 @@ import { Provider } from 'react-redux';
 import reducers from './main/reducers/reducers';
 import promise from 'redux-promise';
 
-const store = applyMiddleware(promise)(createStore)(reducers);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
     <Provider store={store}>
