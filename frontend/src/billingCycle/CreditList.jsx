03df3bd81550d1547cb2ea899a45ca862ad5af0b
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-17 16:17:54 -0300

    29 | Add actions to credit list form

--- frontend/src/billingCycle/CreditList.jsx
+++ frontend/src/billingCycle/CreditList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Field } from 'redux-form';
+import { connect } from 'react-redux';
 import Grid from '../common/layout/Grid';
 import Input from '../common/form/Input';
+import { Field, arrayInsert, arrayRemove } from 'redux-form';
+import { bindActionCreators } from 'redux';
 
 class CreditList extends React.Component {
+    add(index, item = {}) {
+        if(!this.props.readOnly){
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }   
+    }
+
+    remove(index) {
+        if(!this.props.readOnly && this.props.list.length > 1) {
+            this.props.arrayRemove('billingCycleForm', 'credits', index)
+        }
+    }
+
     renderRows() {
         const list = this.props.list || [];
 
@@ -25,7 +39,29 @@ class CreditList extends React.Component {
                         placeholder={'Informe o valor'}
                     />
                 </td>
-                <td></td>
+                <td>
+                    <button 
+                        type={'button'}
+                        className={'btn btn-success'}
+                        onClick={() => this.add(index + 1)}
+                    >
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button 
+                        type={'button'}
+                        className={'btn btn-warning'}
+                        onClick={() => this.add(index + 1, item)}
+                    >
+                        <i className="fa fa-clone"></i>
+                    </button>
+                    <button 
+                        type={'button'}
+                        className={'btn btn-danger'}
+                        onClick={() => this.remove(index)}
+                    >
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                </td>
             </tr>
         ));
     }
@@ -53,4 +89,6 @@ class CreditList extends React.Component {
     }
 }
 
-export default CreditList;
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
+
+export default connect(null, mapDispatchToProps)(CreditList);
