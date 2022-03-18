b2cba0450aa6f7971678b837bde36ceec430e2f1
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-18 14:37:06 -0300

    36 | Develop Auth Actions / Reduce in Frontend

--- frontend/src/main/reducers/reducers.js
+++ frontend/src/main/reducers/reducers.js
@@ -4,12 +4,14 @@ import TabsReducers from "../../common/Tab/tabs.reducers";
 import DashboardReducer from "../../Dashboard/Dashboard.reducer";
 import BillingCycleReducer from "../../billingCycle/BillingCycle.reducer";
 import { reducer as toastrReducer } from "react-redux-toastr";
+import authReducer from "../../auth/auth.reducer";
 
 const rootReducer = combineReducers({
     billingCycle: BillingCycleReducer,
     dashboard: DashboardReducer,
     toastr: toastrReducer,
     form: formReducer,
+    auth: authReducer,
     tab: TabsReducers,
 })
 
