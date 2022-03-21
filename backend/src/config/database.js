28db91b598cd1abc02a244437614f288a46ecb20
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-21 15:32:29 -0300

    40 | Prepare project for deployment

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://localhost/mymoney', 
-    { useNewUrlParser: true }
-);
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minímo de '{MIN}'.";
